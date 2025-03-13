f20ddb3752305b4a1ba8ec5df3c4592d0acb8951
cutedina <aknot2bad@gmail.Com>
2025-03-13 17:37:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,57 +5,52 @@ const buttonGrid = document.querySelector('.button-grid');
 const journalEntries = [
     // Custom entries for first 5 days
     {
-        title: "📖 Day 1: Became an Overnight Author!",
-        content: `<p>Wrote 3 whole pages of my masterpiece "How to Pet a Cat Properly"!<br>
-                  Sadly, my dog ate pages 2 and 3...<br>
-                  <em>New book title:</em> "Why Dogs Shouldn't Proofread"</p>
-                  <p>🐾 Word count: 47 (before canine editing)</p>`
+        title: "    Journal Log 1: The Beginning",
+        content: `<p>Welp, today: March 13th 2025, marks the first day of the challenge, huh… 
+                  <br>I really didn't expect that I’d follow through with it but it didn't sit right with me to do you dirty like that and then for everything to be back to normal without taking at least SOME sort of punishment
+                  <br>Although a bit extreme, I think I can afford to be sad for a month and then be happy later for reallll. 
+                  <br>( with you perhaps??? :3 )
+                  <br>Anyways, gosh knows how sad I was yesterday, I was pretty devastated not gonna lie.
+                  <br>I started thinking of ways I could cope with this, or at least try to convince you that you really are my special pookie wookie, and I landed with making a website :D which is exactly what you’re seeing right now
+                  <br><em>…well</em>, using the word “exactly” is a bit of a stretch, as I have just started developing it so I'm not even sure of the final outcome of it, and I also started taking some coding lessons in three languages: HTML, CSS and Javascript, and I’d like to think that my hard work is beginning to show on godddd.
+                  <br>Enough of that though, the whole point of this blog style website is to fill you in about my life, how I’m doing, etc… to prove to you that no matter how far away we are from each other, there’s always going to be some sort of contact, even if it meant training a pigeon to send you a letter ;).</p/
+                  <br><br>
+                  <br><br>
+                  <br><br>
+                  <p>So, today was certainly a day alright:
+                  <br><br>
+                  <br>For one, I got to see you plenty of times, I was basking in the beauty for what I presumed would be the last time until the next semester, and it was ironic; You were wearing yellow, so you looked like the sun, and even though I wanted to stare for so long and even talk to you, I knew that I shouldn’t and that I couldn’t, but oh well.
+                  <br><br>
+                  <br>We also did an hour and a half of PE, I was really happy because I didn’t expect myself to do that well in Volleyball, as I had decided that it wasn’t for me last year when I barely got to improve and made the ball fly all the way to the roof at the end of year, but I was doing some really good plays with really good precision to boot, and also was no longer scared of the ball. 
+                  <br>My friend who played it with me a lot and tried coaching me before said that he was really proud of me, so I’m happy.
+                  <br>Although my whole body felt sore by the end of it, and I was really thirsty.
+                  <br><br>
+                  <br>I’m not sure if it matters to you by the time you read this, but I got 17 in french, so that’s cool.</p>
+                  <br><br>
+                  <br>We played Uno in class!
+                  <br>After I got kicked out of your class ( totally didn’t come to sneak a lil peek or two or anything nope nope nope ), we formed a party of 13 and started playing the game! We did 2 games and they were hella fun, since we kept playing until we had a loser instead of finishing with the first winner.
+                  <br><em>… I did get my butt handed to me, though…</em> It wasn’t pretty; I got hit with a +12 and immediately after it a +10, not only that, but all my cards were just number cards.
+                  <br>I had no shot at victory with a deck of 20~ cards, so I decided to sabotage the person next to me, who was Amira. Yes, THAT Amira. She had 2 cards left.
+                  <br>I was laughing my butt off at her, I was full on focusing on her and visualizing her cards, and I realized that she only had red and blue cards.
+                  <br>So I put my cards to use and made it hell for her; Either by putting down yellow or green cards, giving her more cards, or downright just cancelling her turn. 
+                  <br>God I love being evil. I did win after that so that was cool.
+                  <br><br>
+                  <br>And then, I walked home with Fares and Nadjib, we were just yapping and yapping about the most random stuff on earth ( mostly Fares bragging about his plays in Werewolf )
+                  <br>My legs hurt a lot whilst walking, though. I already started having major pain by the time we got into school and it was way worse when I walked home.
+                  <br><br>
+                  <br>I did make it home though, so all’s good. ( felt weird not taking a picture but what can I say )
+                  <br>I immediately took a shower because duh. And then I lied down in bed for around an hour to rest
+                  <br><br>
+                  <br>Then I got up, sat my butt down, and started learning to do the website. It's in its infancy stage, of course, it still will display this though!!
+                  <br><br>
+                  <br>It’s like 17:20 at the time, still a long time till I can drink so that’s unfortunate. I’m currently listening to a podcast and writing this stuff, maybe I’ll end it here for today. I miss you a lot and I still love you 
+                  <br>I reckon I should at least add an update at night, since I plan on sleeping in around 8 hours.</p>`
     },
     {
-        title: "👽 Day 2: Made First Contact!",
-        content: `<p>Met a new "friend" in the park! Highlights:</p>
-                  <ul>
-                    <li>Species: Unknown (claims to be human)</li>
-                    <li>Shared snacks: My cookies 🍪, their suspicious green jelly</li>
-                    <li>Conspiracy theories discussed: 12</li>
-                  </ul>
-                  <p>⚠️ Note: Check if local zoo missing any penguins</p>`
-    },
-    {
-        title: "🍝 Day 3: Noodle Emergency!",
-        content: `<p>Ate spaghetti so good I cried actual tears!<br>
-                  Sauce-to-noodle ratio: PERFECTION<br>
-                  Now suffering from:</p>
-                  <ul>
-                    <li>Food coma 😴</li>
-                    <li>Urge to learn Italian</li>
-                    <li>Fear I'll never taste this good again</li>
-                  </ul>`
-    },
-    {
-        title: "🦜 Day 4: Bird Negotiations",
-        content: `<p>Pigeon demanded parking fee for my balcony.<br>
-                  Negotiation attempts:</p>
-                  <ul>
-                    <li>Offered bread crumbs ➡️ rejected</li>
-                    <li>Tried bird impressions ➡️ offended them</li>
-                    <li>Final agreement: My left slipper as ransom</li>
-                  </ul>`
-    },
-    {
-        title: "🎸 Day 5: Rockstar Dreams",
-        content: `<p>Air-guitared to Queen for 2 hours straight!<br>
-                  Achievements unlocked:</p>
-                  <ul>
-                    <li>Neighbors filed noise complaint (silent version!)</li>
-                    <li>Invented new dance move: "The Shredding Flamingo"</li>
-                    <li>Guitar-shaped pizza ordered to celebrate</li>
-                  </ul>`
-    },
     // Placeholder entries for days 6-30
-    ...Array(25).fill().map((_, i) => ({
-        title: `🌼 Day ${i + 6} 🌸`,
-        content: `Today was an amazing day! ${'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '.repeat(5)}`
+    ...Array(29).fill().map((_, i) => ({
+        title: `🌼 Day ${i + 2} 🌸`,
+        content: `Today was an amazing day! or something idk yet`
     }))
 ];
 
